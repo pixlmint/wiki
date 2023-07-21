09fd2bf668451bd398bfaec6cf190b5ad2b1fc53
pixlmint <christian.groeber@bluewin.ch>
2023-07-21 18:06:30 +0200

    fixed creating index.md file

--- docker/startup.sh
+++ docker/startup.sh
@@ -3,7 +3,7 @@ chown -R wiki_user:users data content media
 
 file=content/index.md
 
-if [[ -f "$file" ]]; then
+if [[ ! -f "$file" ]]; then
   echo '---' > content/index.md
   echo 'title: Your New Wiki' >> content/index.md
   echo '---' >> content/index.md
