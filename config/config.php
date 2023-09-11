427b9ab6b4127a2a917714e2783fb801042ba988
ChristianGroeber <swiss8oy.chg@gmail.com>
2023-09-11 08:57:01 +0200

    added language tag

--- config/config.php
+++ config/config.php
@@ -19,7 +19,7 @@ return [
         ],
     ],
     'wikiFrontend' => [
-        'version' => '1.7',
+        'version' => '1.7.1',
     ],
     'base' => [
         'frontendController' => FrontendController::class,
