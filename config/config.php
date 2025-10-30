996f3038141baa6e2735d7050665b7e6e755a293
Christian Gröber <swiss8oy.chg@gmail.com>
2025-10-30 13:19:35 +0100

    v1.21.3

--- config/config.php
+++ config/config.php
@@ -44,7 +44,7 @@ return [
         ],
     ],
     'wikiFrontend' => [
-        'version' => '1.21.2',
+        'version' => '1.21.3',
     ],
     'base' => [
         'frontendController' => FrontendController::class,
